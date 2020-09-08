44bfe71c8e4e28c18bcb05a5c4703fae9cfacaf2
Danylo Oknemifort <ultrashelter@gmail.com>
2020-09-08 18:05:56 +0200

    update 2.1

--- splide.js
+++ splide.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#image-slider', {
-        type: 'loop',
-        perPage: 2,
         autoplay: true,
     }).mount();
 });
